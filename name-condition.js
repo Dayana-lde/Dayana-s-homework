a585dd1eba7f7f311b8f767a82addbb7f08915fc
Dayana-lde <lutsay.dayana@gmail.com>
2025-02-06 20:11:02 +0500

    Create name-condition.js

---
+++ name-condition.js
@@ -0,0 +1,9 @@
+let name = prompt('what s your name?', ' ');
+if (name == 'Zhasmin'){
+    alert('сегодня твой счастливый день! получи свой приз.');
+} else if (name == 'Aidana'){
+    alert('сегодня и твой день! получи скидку');
+}
+else{
+    alert('добро пожаловать, ', name);
+}
