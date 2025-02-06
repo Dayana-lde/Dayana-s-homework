41d1d94e9b82f2960557784ee6d478b8f2926921
Dayana-lde <lutsay.dayana@gmail.com>
2025-02-06 20:18:56 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+function wearACoat(degree){
+    if (degree <= 50){
+    console.log('wear a coat');
+    }
+    else if(degree <= 30){
+        console.log('wear a coat and a hat');
+    }
+    else if (degree <= 0){
+        console.log('stay inside');
+    }
+    else{
+        console.log('wear whatever you want');
+    }
+ }
+ wearACoat(50);
+ wearACoat(30);
+ wearACoat(0);
+ wearACoat(50);
