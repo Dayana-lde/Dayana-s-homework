d91a300a821feca7b200cbf756a624e4a2af0909
Dayana-lde <lutsay.dayana@gmail.com>
2025-02-06 20:14:43 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+et condition = prompt('enter condition fot being president:', ' ');
+if (condition == 'i have helped more that 50 people'){
+    alert('you are in');
+} else if (condition == 'i studied for deputy' || condition == 'i love nature'){
+    alert('you are in');
+}
