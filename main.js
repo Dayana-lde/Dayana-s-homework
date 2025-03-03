cc7496ef533b51844913ea9a035b82b4fae9fe5f
Dayana-lde <lutsay.dayana@gmail.com>
2025-03-03 12:43:59 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const paragraph =
   " the quick brown box jumps over the lazy dog. if the dog barked, was it really lazy?";
 const searchTerm = "dog";
-console.log(paragraph.indexOf(searchTerm));
-console.log(paragraph.lastIndexOf(searchTerm));
+const first = paragraph.indexOf(searchTerm);
+const second = paragraph.indexOf(searchTerm, first + 1);
+console.log(first, second);
