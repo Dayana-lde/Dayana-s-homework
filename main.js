697c7dd6761ff94988bff7de03d79fd7c9ba64cf
Dayana-lde <lutsay.dayana@gmail.com>
2025-03-03 13:52:27 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 function wearACoat(degree){
-  if (degree > 30){
-  console.log('wear a coat');
+  if (degree < 0){
+  console.log('stay inside');
   }
-  else if(degree > 0){
+  else if(degree < 30){
       console.log('wear a coat and a hat');
   }
-  else if (degree === 0){
-      console.log('stay inside');
+  else if (degree < 50){
+      console.log('wear a coat');
   }
   else{
       console.log('wear whatever you want');
   }
 }
 wearACoat(50);
-wearACoat(30);
-wearACoat(0);
-wearACoat(50);
+wearACoat(40);
+wearACoat(10);
+wearACoat(-7);
+
+// я поставила температуру в другом порядке, теперь не будет постоянного значения wear a coat
