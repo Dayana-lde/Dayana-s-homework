49f46c1ad0a45e7e18cae94f9f1b4c0c6bdd6a2f
Dayana-lde <lutsay.dayana@gmail.com>
2025-03-03 13:44:58 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-let condition = prompt('enter condition fot being president:', ' ');
+
+function goForPresident(condition){
 if (condition === 'Citizen of Kazakhstan by birth'){
-    alert('you are in');
+    console.log('you are in');
 } else if (condition === 'fluent in the state language' || condition === 'I have a higher education'){
-    alert('you are in');
+    console.log('you are in');
 } else if (condition === 'over 40'){
-    alert('you are in');
+    console.log('you are in');
+} else{
+  console.log("i'm sorry you aren't allowed go for president")
 }
+}
+
+goForPresident("Citizen of Kazakhstan by birth");
+goForPresident("fluent in the state language");
+goForPresident("over 40");
+goForPresident("over 30");
