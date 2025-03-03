eb78fbd39836035d086dc03d8f19ec7be9be2ae7
Dayana-lde <lutsay.dayana@gmail.com>
2025-03-03 13:34:59 +0600

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function word(smth) {
   let word1 = smth.toString().split('');
   word1.reverse();
   let word2 = word1.join('');
-if (smth.toString() === word2) {
+if (smth.toString().toUpperCase() === word2.toUpperCase() ) {
       console.log("word is palindrome");
   } else {
       console.log("that is not a palindrome");
