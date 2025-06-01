32a9b106aaa5ea12282dc73184b5a23499662412
Dayana-lde <lutsay.dayana@gmail.com>
2025-06-01 16:10:49 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Timer from './Timer';
+
+function App() {
+  return (
+    <div>
+      <h1>Timer</h1>
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
